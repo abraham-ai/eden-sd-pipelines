7a5a9d0b147d6cbba2242d8db33565d2ff6113e3
Gene Kogan <kogan.gene@gmail.com>
2023-03-11 00:38:20 -0800

    update tests

--- tests/server/run_tests.sh
+++ tests/server/run_tests.sh
@@ -5,7 +5,7 @@ curl -s -X POST \
   "http://0.0.0.0:5000/predictions"
 
 curl -s -X POST \
-  -d '{"version": "fef11678ae5dc4cc024f8d3b4860c65124118a015fecac10fb2f0d652c7538d4", "input": {"mode": "real2real", "text_input": "real2real", "interpolation_seeds": "5|8", "interpolation_init_images":"https://generations.krea.ai/images/928271c8-5a8e-4861-bd57-d1398e8d9e7a.webp|https://generations.krea.ai/images/865142e2-8963-47fb-bbe9-fbe260271e00.webp", "seed": 4, "width": 512, "height": 512, "n_film":1, "n_frames": 20, "smooth": 1, "loop": 1}}' \
+  -d '{"version": "fef11678ae5dc4cc024f8d3b4860c65124118a015fecac10fb2f0d652c7538d4", "input": {"mode": "real2real", "text_input": "real2real", "interpolation_seeds": "5|8", "interpolation_init_images":"https://generations.krea.ai/images/928271c8-5a8e-4861-bd57-d1398e8d9e7a.webp|https://generations.krea.ai/images/865142e2-8963-47fb-bbe9-fbe260271e00.webp", "seed": 4, "width": 512, "height": 512, "steps": 15, "n_film": 1, "n_frames": 20, "smooth": 1, "loop": 1}}' \
   -H "Authorization: Token $REPLICATE_API_TOKEN" \
   -H 'Content-Type: application/json' \
   "http://0.0.0.0:5000/predictions"
@@ -15,7 +15,7 @@ curl -s -X POST -d '{"version": "fef11678ae5dc4cc024f8d3b4860c65124118a015fecac1
   -H 'Content-Type: application/json' \
   "http://0.0.0.0:5000/predictions"
 
-curl -s -X POST -d '{"version": "fef11678ae5dc4cc024f8d3b4860c65124118a015fecac10fb2f0d652c7538d4", "input": {"mode": "interpolate", "text_input": "lerp", "interpolation_seeds": "5|8|13", "interpolation_texts": "hello|world|cats and dogs", "seed": 3, "width": 512, "height": 512, "n_film": 1, "n_frames": 20, "smooth": 1, "loop": 1, "stream": 1, "stream_every": 1}}' \
+curl -s -X POST -d '{"version": "fef11678ae5dc4cc024f8d3b4860c65124118a015fecac10fb2f0d652c7538d4", "input": {"mode": "interpolate", "text_input": "lerp", "interpolation_seeds": "5|8|13", "interpolation_texts": "hello|world|cats and dogs", "seed": 3, "width": 512, "height": 512, "n_film": 1, "n_frames": 20, "steps": 15, "smooth": 1, "loop": 1, "stream": 1, "stream_every": 1}}' \
   -H "Authorization: Token $REPLICATE_API_TOKEN" \
   -H 'Content-Type: application/json' \
   "http://0.0.0.0:5000/predictions"
