ac0d9831bf95d0fe7907a12725fb81fbe49098a9
Gene Kogan <kogan.gene@gmail.com>
2023-06-16 21:34:29 -0700

    send back multiple samples

--- tests/server/run_cog_server.sh
+++ tests/server/run_cog_server.sh
@@ -1,3 +1,1 @@
-# docker run -p 5000:5000 --gpus=all r8.im/abraham-ai/eden-sd-pipelines
 docker run -p 5000:5000 --gpus=all -v /home/gene/dev/eden-sd-pipelines/:/src r8.im/abraham-ai/eden-sd-pipelines
-
