8e462094d3d5f71a59888ed17fce92bc6bbc6634
xander <xandersteenbruggex@gmail.com>
2023-08-25 02:40:21 -0700

    activate controlnet test

--- tests/generate_controlnet.sh
+++ tests/generate_controlnet.sh
@@ -1,2 +1,2 @@
-cog predict -i mode=generate -i text_input="An incredible galactic starfleet ship, travelling the galaxy and passing the planet Saturn" -i seed=5 -i width=1024 -i height=1024 -i steps=40 -i upscale_f=1.5 -i controlnet_type="off" -i init_image_data="https://generations.krea.ai/images/3cd0b8a8-34e5-4647-9217-1dc03a886b6a.webp" -i init_image_strength=0.6
+cog predict -i mode=generate -i text_input="An incredible galactic starfleet ship, travelling the galaxy and passing the planet Saturn" -i seed=5 -i width=1024 -i height=640 -i steps=40 -i upscale_f=1.5 -i controlnet_type="canny-edge" -i init_image_data="https://generations.krea.ai/images/3cd0b8a8-34e5-4647-9217-1dc03a886b6a.webp" -i init_image_strength=0.6
 
