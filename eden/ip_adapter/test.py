7f9397e885df882e1704d8c3b65759dd92c3dfaf
xander <xandersteenbruggex@gmail.com>
2023-10-04 11:05:25 -0700

    add ip_adapter

---
+++ eden/ip_adapter/test.py
@@ -0,0 +1,85 @@
+import torch
+from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
+from PIL import Image
+import os, random, time
+from ip_adapter import IPAdapterXL, IPAdapterPlusXL
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def prep_image(image, target_res = 1024, pad = True):
+    w, h = image.size
+
+    if pad:
+        square_canvas = Image.new('RGB', size=(max(w, h), max(w, h)))
+        square_canvas.paste(image, box=((max(w, h)-w)//2, (max(w, h)-h)//2))
+    else: # simply crop the center square:
+        square_canvas = image.crop(box=((w-h)//2, 0, (w+h)//2, h))
+
+    # resize to target size:
+    image = square_canvas.resize((target_res, target_res), resample=Image.LANCZOS)
+
+    return image
+
+def image_grid(imgs, rows, cols, grid_w = 1024):
+    assert len(imgs) == rows*cols
+    grid = Image.new('RGB', size=(cols*grid_w, rows*grid_w))
+
+    for i, img in enumerate(imgs):
+        img = prep_image(img, target_res=grid_w, pad = True)
+        grid.paste(img, box=(i%cols*grid_w, i//cols*grid_w))
+    return grid
+
+
+if 0:
+    base_model_path = "/data/xander/Projects/cog/eden-sd-pipelines/models/checkpoints/sdxl-v1.0/sd_xl_base_1.0_0.9vae.safetensors"
+    pipe = StableDiffusionXLImg2ImgPipeline.from_single_file(
+        base_model_path,
+        torch_dtype=torch.float16,
+        add_watermarker=False).to(device)
+else:
+    base_model_path = "stabilityai/stable-diffusion-xl-base-1.0"
+    pipe = StableDiffusionXLPipeline.from_pretrained(
+        base_model_path,
+        torch_dtype=torch.float16,
+        add_watermarker=False,
+    ).to(device)
+
+#pipe_output = pipe(prompt="hello world", num_inference_steps=35).images
+
+image_encoder_path = "/data/xander/Projects/cog/eden-sd-pipelines/models/ip_adapter/image_encoder"
+ip_ckpt = "/data/xander/Projects/cog/eden-sd-pipelines/models/ip_adapter/ip-adapter_sdxl.bin"
+ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device)
+
+img_folder = "/data/xander/Projects/cog/stable-diffusion-dev/eden/xander/init_imgs/test"
+prompts = [
+    "cubism",
+    "impressionist painting",
+    "cyberpunk",
+    "on the beach",
+    "Zeiss 150mm f/2.8, highly detailed, sharp",
+    "logo design",
+    "🦋",
+    "🔥",
+]
+
+random.seed(int(time.time()))
+num_samples = 1
+os.makedirs("results", exist_ok=True)
+
+for i in range(20):
+    # grab a random img from the folder:
+    image_path = os.path.join(img_folder, random.choice(os.listdir(img_folder)))
+    image  = prep_image(Image.open(image_path))
+    prompt = random.choice(prompts)
+
+    # generate image variations with only image prompt
+    images = ip_model.generate(pil_image=image, num_samples=num_samples, num_inference_steps=30, seed=420)
+    grid = image_grid([image] + images, 1, num_samples+1)
+    grid.save(f"results/variations_{i:03d}_{int(time.time())}.jpg", quality=95)
+
+    # multimodal prompts
+    image_scale = random.uniform(0.4, 0.7)
+    images = ip_model.generate(pil_image=image, num_samples=num_samples, num_inference_steps=30, seed=420, prompt=prompt, scale=image_scale)
+    grid = image_grid([image] + images, 1, num_samples+1)
+    safe_prompt = prompt.replace("/", "_")
+    grid.save(f"results/variations_{image_scale:.2f}_{i:03d}_{int(time.time())}_prompt_{safe_prompt}.jpg", quality=95)
